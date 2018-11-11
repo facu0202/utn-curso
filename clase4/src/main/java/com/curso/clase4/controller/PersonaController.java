63f3654fe671d98b2df14ec0cae5aca13f457e30
facundoferro <facundoferro@gmail.com>
2018-11-11 14:06:55 -0300

    se suben nueva version

--- clase4/src/main/java/com/curso/clase4/controller/PersonaController.java
+++ clase4/src/main/java/com/curso/clase4/controller/PersonaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.curso.clase4.domain.Persona;
 import com.curso.clase4.repository.PersonaRepository;
+import com.curso.clase4.servicio.ServicioEjemplo;
 
 @Controller
 @RequestMapping(value="personas")
@@ -18,11 +19,24 @@ public class PersonaController {
 	@Autowired
 	PersonaRepository personaRepository; 
 	
+	@Autowired
+	ServicioEjemplo servicioEjemplo; 
+	
+
+	
 	@RequestMapping(value="/crear")
 	public String crearForm(Model model)
 	{
 		model.addAttribute("persona", new Persona());
 		return "persona/crear"; 
+	}
+	
+	@RequestMapping(value="/crear",method = RequestMethod.POST)
+	public String modificarEnd(Model model,@ModelAttribute Persona persona, BindingResult bindingResult)
+	{
+		personaRepository.save(persona);
+		
+		return "redirect:"; 
 	}
 	
 	@RequestMapping(value="/modificar/{id}")
@@ -51,7 +65,19 @@ public class PersonaController {
 	@RequestMapping(value="/")
 	public String listar(Model model)
 	{
+		Persona p = new Persona();
+		p.setApellido("Fulanito");
+		p.setNombre("nombre");
+		
 		model.addAttribute("personas", personaRepository.findAll());
+		model.addAttribute("hola",p);
+		return "persona/list";
+	}
+	
+	@RequestMapping(value="/testservicio")
+	public String testservicio(Model model)
+	{
+		servicioEjemplo.ejemplo();
 		return "persona/list";
 	}
 }
