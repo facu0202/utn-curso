cd6abbbbd89116d0de9414ff79f75f3c8d6d26f8
facundoferro <facundoferro@gmail.com>
2018-11-21 19:09:46 -0300

    Add new project

---
+++ calendar/src/main/java/com/free_calendar_system/calendar/CalendarApplication.java
@@ -0,0 +1,12 @@
+package com.free_calendar_system.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarApplication.class, args);
+	}
+}
